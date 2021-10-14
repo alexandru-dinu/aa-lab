7f625b5d890d26e198d51a49b2f17264476cf747
Alexandru Dinu <alex.dinu07@gmail.com>
2021-10-14 11:23:28 +0300

    Reformat file.

--- misc/railway.c
+++ misc/railway.c
@@ -17,140 +17,139 @@ by travelling through each wagon and operating the light switches.
 
 #define len(X) (sizeof(X) / sizeof(X[0]))
 
-int const states[] = {0, 1, 1, 0};
+int const states[] = { 0, 1, 1, 0 };
 
-typedef struct _node {
-	int value;
-	struct _node *next;
-	struct _node *prev;
+typedef struct _node
+{
+    int value;
+    struct _node *next;
+    struct _node *prev;
 } node_t, *list_t;
 
 list_t cons(int value, list_t prev, list_t next)
 {
-	list_t node = calloc(1, sizeof(node_t));
+    list_t node = calloc(1, sizeof(node_t));
 
-	node->value = value;
-	node->prev = prev;
-	node->next = next;
+    node->value = value;
+    node->prev = prev;
+    node->next = next;
 
-	return node;
+    return node;
 }
 
 list_t make_railway()
 {
-	int i;
-	list_t wagons = NULL;
-	list_t last = NULL;
-	list_t first = NULL;
+    int i;
+    list_t wagons = NULL;
+    list_t last = NULL;
+    list_t first = NULL;
 
-	wagons = cons(states[len(states) - 1], NULL, NULL);
-	first = wagons;
+    wagons = cons(states[len(states) - 1], NULL, NULL);
+    first = wagons;
 
-	for (i = len(states) - 2; i >= 0; i--) {
-		list_t aux = cons(states[i], wagons, NULL);
-		wagons->next = aux;
-		wagons = aux;
-	}
+    for (i = len(states) - 2; i >= 0; i--) {
+        list_t aux = cons(states[i], wagons, NULL);
+        wagons->next = aux;
+        wagons = aux;
+    }
 
-	// make circular
-	wagons->next = first;
-	first->prev = wagons;
+    // make circular
+    wagons->next = first;
+    first->prev = wagons;
 
-	return first;
+    return first;
 }
 
 void print(list_t wagons)
 {
-	int i = 0;
-	list_t aux = wagons;
+    int i = 0;
+    list_t aux = wagons;
 
-	while (1) {
-		printf("[%d]\n", aux->value);
+    while (1) {
+        printf("[%d]\n", aux->value);
 
-		if ((++i) % len(states) == 0)
-			break;
-		aux = aux->next;
-	}
+        if ((++i) % len(states) == 0)
+            break;
+        aux = aux->next;
+    }
 }
 
 int count(list_t wagons, int start_wagon)
 {
-	int count = 0;
-	int offset_from_beginning = 0;
-	int i;
+    int count = 0;
+    int offset_from_beginning = 0;
+    int i;
 
-	// the idea is to have only one ptr
-	// i.e. only one person in the train
-	list_t w = wagons;
+    // the idea is to have only one ptr
+    // i.e. only one person in the train
+    list_t w = wagons;
 
-	// go to the start wagon
-	for (i = 0; i < start_wagon; i++)
-		w = w->next;
+    // go to the start wagon
+    for (i = 0; i < start_wagon; i++)
+        w = w->next;
 
-	// turn off the light in the first wagon
-	// and move to the next one
-	w->value = 0;
-	w = w->next;
-	offset_from_beginning++;
-	count++;
+    // turn off the light in the first wagon
+    // and move to the next one
+    w->value = 0;
+    w = w->next;
+    offset_from_beginning++;
+    count++;
 
-	while (1) {
-		// keep looking
-		if (w->value == 1) {
-			offset_from_beginning++;
-			count++;
-			w = w->next;
-		}
-		else {
-			// turn on the light in the current wagon
-			w->value = 1;
+    while (1) {
+        // keep looking
+        if (w->value == 1) {
+            offset_from_beginning++;
+            count++;
+            w = w->next;
+        } else {
+            // turn on the light in the current wagon
+            w->value = 1;
 
-			// go back to wagon 1
-			for (i = 0; i < offset_from_beginning; i++)
-				w = w->prev;
+            // go back to wagon 1
+            for (i = 0; i < offset_from_beginning; i++)
+                w = w->prev;
 
-			// if the light in the starting wagon is on,
-			// then all wagons were traversed
-			if (w->value == 1)
-				return count;
-			else
-				// continue looking from where we left
-				for (i = 0; i < offset_from_beginning; i++)
-					w = w->next;
-		}
-	}
+            // if the light in the starting wagon is on,
+            // then all wagons were traversed
+            if (w->value == 1)
+                return count;
+            else
+                // continue looking from where we left
+                for (i = 0; i < offset_from_beginning; i++)
+                    w = w->next;
+        }
+    }
 
-	// unlikely
-	return -1;
+    // unlikely
+    return -1;
 }
 
 void check(int count)
 {
-	if (count == len(states))
-		printf("CORRECT\n");
-	else
-		printf("WRONG\n");
+    if (count == len(states))
+        printf("CORRECT\n");
+    else
+        printf("WRONG\n");
 }
 
 int main(int argc, char const *argv[])
 {
-	int i;
+    int i;
 
-	list_t wagons = make_railway();
+    list_t wagons = make_railway();
 
-	//print(wagons);
+    // print(wagons);
 
-	// consider each wagon as a start wagon
-	for (i = 0; i < len(states); i++)
-		check(count(wagons, i));
+    // consider each wagon as a start wagon
+    for (i = 0; i < len(states); i++)
+        check(count(wagons, i));
 
+    // free memory
+    for (i = 0; i < len(states); i++) {
+        list_t aux = wagons;
+        wagons = wagons->next;
+        free(aux);
+    }
 
-	// free memory
-	for(i = 0; i < len(states); i++) {
-		list_t aux = wagons;
-		wagons = wagons->next;
-		free(aux);
-	}
-
-	return 0;
+    return 0;
 }
